42f3e1241b80e9ec474c20046fb680112a272e26
MichaelBaloge <139211557+MichaelBaloge@users.noreply.github.com>
2023-07-24 18:02:48 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 import streamlit as st
 import pandas as pd
-import json
 import seaborn as sns
 import matplotlib.pyplot as plt
 import requests
@@ -104,13 +103,13 @@ update_url = 'http://mbcreditmodelapi.azurewebsites.net/update'
 def get_prediction(row_number):
     data = {'data': row_number}
     response = requests.post(predict_url, json=data)
-    return json.loads(response),response.status_code
+    return response,response.status_code
 
 # fonction d'envoi des mises à jour et de récupération des nouvelles prédictions
 def submit_data(dico):
     data = {'data': dico}
     response = requests.post(update_url, json=data)
-    return json.loads(response),response.status_code
+    return response,response.status_code
 
 # fonction de callback de la sélection du client (entrée pour vérification de la validité)
 def callback1():
